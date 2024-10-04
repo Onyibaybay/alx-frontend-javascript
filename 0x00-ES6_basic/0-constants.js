066481e9cd6cac962c35cee08e6cff0cc0190487
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-04 22:33:03 +0000

    o-constas

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,11 +1,18 @@
-// Function taskFirst uses const
-function taskFirst() {
+// Modify taskFirst to use const
+export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Function taskNext uses let
-function taskNext() {
-  let task = 'But sometimes let is okay.';
-  return task;
+// No changes needed for getLast
+export function getLast() {
+  return ' is okay';
+}
+
+// Modify taskNext to use let
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
