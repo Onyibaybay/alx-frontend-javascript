60912ee4674035aa4ec704624926dd345b747b0f
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-04 22:14:48 +0000

    o-constants

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,11 +1,11 @@
-// Modify taskFirst to use const
+// Function taskFirst uses const
 function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Modify taskNext to use let
+// Function taskNext uses let
 function taskNext() {
-  let task = 'But sometimes let is okay';
+  let task = 'But sometimes let is okay.';
   return task;
 }
