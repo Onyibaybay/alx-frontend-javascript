815bb7db8d0e99d2b1d339ccc9762e509f8fbba2
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-04 22:56:33 +0000

    arrow

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -7,7 +7,3 @@ export default function getNeighborhoodsList() {
     return self.sanFranciscoNeighborhoods;
   };
 }
-import getNeighborhoodsList from './2-arrow.js';
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
