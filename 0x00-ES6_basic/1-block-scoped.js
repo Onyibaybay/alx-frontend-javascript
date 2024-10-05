401ac87091b3dbce0ca3b128dfcf6ac39c27882d
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-05 01:31:40 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Scoped only within the if block
-    let task2 = false; // Scoped only within the if block
+    const task = true;  // Scoped only within the if block
+    const task2 = false; // Scoped only within the if block
   }
 
-  return [task, task2];
+  return [task, task2]; // The outer task and task2 are returned
 }
