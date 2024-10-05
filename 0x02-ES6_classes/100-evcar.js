db02c3846295bb804ca3a7ec4c8d318fc90b8da8
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-05 03:05:02 +0000

    100 evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,30 @@
+import Car from './10-car.js';
+
+const _range = Symbol('range');
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    // Call the parent class constructor
+    super(brand, motor, color);
+    
+    // Verify that range is a string
+    if (typeof range !== 'string') {
+      throw new TypeError('range must be a string');
+    }
+
+    // Store range attribute in a symbol-prefixed variable
+    this[_range] = range;
+  }
+
+  // Override the cloneCar method to return a Car instance
+  cloneCar() {
+    return new Car(this.brand, this.motor, this.color);
+  }
+
+  // Getter for the range attribute
+  get range() {
+    return this[_range];
+  }
+}
+
+export default EVCar;
