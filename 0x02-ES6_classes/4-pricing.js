a3805d80723eac170a6071c775c3c26c052ab36b
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-05 02:36:51 +0000

    4 pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,59 @@
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    // Verify types of attributes
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+
+    // Store attributes in underscore-prefixed variables
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and Setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter and Setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
