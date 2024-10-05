0b892e966fdc332202a99e3a66e281779c0e202f
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-05 02:43:01 +0000

    6 sky

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,31 @@
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Assign sqft to the parent class
+    // Verify that floors is a number
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+
+    // Store attributes in underscore-prefixed variables
+    this._floors = floors;
+  }
+
+  // Getter for sqft (inherited from Building)
+  get sqft() {
+    return super.sqft; // Calls the getter from the Building class
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
+
+export default SkyHighBuilding;
