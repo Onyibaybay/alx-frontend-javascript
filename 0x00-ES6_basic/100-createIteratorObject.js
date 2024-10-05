4d90e82ed4236eacad5712e47e225e1a04d52e96
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-05 00:46:39 +0000

    100 create

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  
+  // Combine all employees from all departments into a single array
+  for (const department of Object.values(report.allEmployees)) {
+    allEmployees = [...allEmployees, ...department];
+  }
+
+  // Return an iterator for the combined array of employees
+  return allEmployees[Symbol.iterator]();
+}
+
