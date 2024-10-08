1735e94d0fb68faff21df563ecf15eb6512382c2
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-08 14:50:08 +0000

    7airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,32 +1,32 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
-    // Verify that name and code are strings
-    if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
-    }
-
-    // Store attributes in underscore-prefixed variables
-    this._name = name;
-    this._code = code;
+    this.name = name;
+    this.code = code;
   }
 
-  // Getter for name
-  get name() {
-    return this._name;
-  }
-
-  // Getter for code
   get code() {
     return this._code;
   }
 
-  // Default string description of the class
-  toString() {
-    return this._code;
+  set code(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
   }
 }
-
-export default Airport;
