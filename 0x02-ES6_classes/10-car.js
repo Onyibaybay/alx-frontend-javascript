0cda2acfda79d51e75b15b231b207f0c4720ea91
onyibaybay <onyinyechukwuedith090@gmail.com>
2024-10-08 15:10:56 +0000

    class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,44 +1,41 @@
-// Create unique symbols for each attribute
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
-    // Verify that inputs are of the correct type
-    if (typeof brand !== 'string') {
-      throw new TypeError('brand must be a string');
-    }
-    if (typeof motor !== 'string') {
-      throw new TypeError('motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw new TypeError('color must be a string');
-    }
-
-    // Store attributes in symbol-prefixed variables
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
-  // Method to clone the car
-  cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
-  }
-
-  // Getters for each attribute
   get brand() {
-    return this[_brand];
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
   }
 
   get motor() {
-    return this[_motor];
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
   }
 
   get color() {
-    return this[_color];
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    const clone = new Species();
+    return clone;
   }
 }
-
-export default Car;
--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,30 +1,21 @@
 import Car from './10-car';
 
-const _range = Symbol('range');
-
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // Call the parent class constructor
     super(brand, motor, color);
-
-    // Verify that range is a string
-    if (typeof range !== 'string') {
-      throw new TypeError('range must be a string');
-    }
-
-    // Store range attribute in a symbol-prefixed variable
-    this[_range] = range;
+    this.range = range;
   }
 
-  // Override the cloneCar method to return a Car instance
-  cloneCar() {
-    return new Car(this.brand, this.motor, this.color);
-  }
-
-  // Getter for the range attribute
   get range() {
-    return this[_range];
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+    return Reflect.construct(Species, []);
   }
 }
-
-export default EVCar;
